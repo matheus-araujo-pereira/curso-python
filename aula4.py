647044b7cc22748461ebfc2641b3da7c9bdbf8e9
Matheus Araujo Pereira <matheusaraujopereira@outlook.com.br>
2024-11-12 21:20:09 -0300

    aulas 4 e 5

---
+++ aula4.py
@@ -0,0 +1,34 @@
+"""
+Tipos int e float
+
+int -> número inteiro
+
+positivo ou negativo
+"""
+print(1)
+print(-1)
+print(0)
+
+"""
+float -> número com ponto flutuante
+
+O tipo float representa qualquer número
+
+positivo ou negativo
+"""
+print(1.1)
+print(1.9)
+print(1.0)
+print(-1.5)
+print(0.5)
+
+"""
+A função type mostra o tipo que o python inferiu ao valor
+"""
+print(type(1))
+print(type(-1))
+print(type(1.0))
+print(type(-1.0))
+print(type('Matheus'))
+print(type("Matheus Araujo Pereira"))
+print(type(1), type(1.0), type('Matheus'), type("Araujo Pereira"))
---
+++ aula5.py
@@ -0,0 +1,6 @@
+# Boolean -> So existem 2 possibilidades -> True ou False
+print(10 == 10) # '==' questiona a igualdade entre os valores
+print(10 == 11)
+print(type(10 == 11))
+print(type(True))
+print(type(False))
