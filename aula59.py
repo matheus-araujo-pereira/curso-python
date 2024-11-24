6952766c5fe6573ea1653092c3d38e7bc908c655
Matheus Araujo Pereira <matheusaraujopereira@outlook.com.br>
2024-11-24 12:08:35 -0300

    Aula 59

---
+++ aula59.py
@@ -0,0 +1,14 @@
+# Desempacotamento em chamadas de métodos e funções
+
+alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+nomes = ["Matheus", "Tahiane", "Ari"]
+dados_matheus = "Matheus", 23, 2001, "Engenharia de Computação - UFS"
+
+for letra in alfabeto:
+    print(letra)
+
+for nome in nomes:
+    print(nome)
+
+for dado in dados_matheus:
+    print(dado)
